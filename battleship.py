ddd516204d46b2f0739a5b7e01dac152bebe69aa
jbarn35 <lqnk3523@gmail.com>
2025-06-20 09:40:36 -0400

    ^_^
    i love mark

--- battleship.py
+++ battleship.py
@@ -1,5 +1,6 @@
 import RPi.GPIO as GPIO
 import random
+import time
 
 TERRITORY = [
             [[4, 17], [23, 18], [21, 20]], 
@@ -20,6 +21,23 @@ EXPANSION2 = [
             [[0,0], [0,0], [0,0]]               
         ]
 
+EXPANSION1EnemeyBoard = [
+                        [[0,0], [0,0], [0,0]],
+                        [[0,0], [0,0], [0,0]],
+                        [[0,0], [0,0], [0,0]]               
+                        ]
+
+EXPANSION2EnemeyBoard = [
+                        [[0,0], [0,0], [0,0]],
+                        [[0,0], [0,0], [0,0]],
+                        [[0,0], [0,0], [0,0]]               
+                        ]
+
+def wait():
+    for i in range(3, 1, -1):
+        print("Next turn in ", i)
+        time.sleep(1)
+
 def printBoard(ledPins, playerBoard):
     for yAxis in range(3):
         for xAxis in range(3):
@@ -33,8 +51,6 @@ def printBoard(ledPins, playerBoard):
             GPIO.output(redLed, playerBoard[yAxis][xAxis][0])
             GPIO.output(greenLed, playerBoard[yAxis][xAxis][1])
 
-
-
 def convertLetterNum(letter):
     if letter == "a" or letter == "A":
         return 0
@@ -42,11 +58,6 @@ def convertLetterNum(letter):
         return 1
     else:
         return 2
-
-def matrix():
-
-
-    print()
 
 #ship placement
 def redefinition(player):
@@ -113,10 +124,39 @@ def redefinition(player):
                     EXPANSION2.append(dark)
 
 #running the game loop
-def pleiades():
-    print()
+def pleiades(player):
+    counter = 0
+    while True:
+        if player == "Player 1":
+            playerID = "ciel"
+            printBoard(EXPANSION2EnemeyBoard)
+            atk(EXPANSION2, EXPANSION2EnemeyBoard)
+            printBoard(EXPANSION2EnemeyBoard)
+            if counter >= 3:
+                game_set(playerID, EXPANSION2)
+            wait()
+            playerID = "tempest"
+            printBoard(EXPANSION1EnemeyBoard)
+            atk(EXPANSION1, EXPANSION1EnemeyBoard)
+            printBoard(EXPANSION1EnemeyBoard)
+            if counter >= 3:
+                game_set(playerID, EXPANSION1)
 
-
+        else:
+            playerID = "tempest"
+            printBoard(EXPANSION1EnemeyBoard)
+            atk(EXPANSION1, EXPANSION1EnemeyBoard)
+            printBoard(EXPANSION1EnemeyBoard)
+            if counter >= 3:
+                game_set(playerID, EXPANSION1)
+            wait()
+            playerID = "ciel"
+            printBoard(EXPANSION2EnemeyBoard)
+            atk(EXPANSION2, EXPANSION2EnemeyBoard)
+            printBoard(EXPANSION2EnemeyBoard)
+            if counter >= 3:
+                game_set(playerID, EXPANSION2)
+        counter += 1
 def game_set():
     print()
 
@@ -168,30 +208,38 @@ def coin_toss():
         return "Player 2"
 
 # Combat functions
-
-def atk(board):
+def atk(board, seenBoard):
     rbd = True
 
     while rbd == True:
-        userShot = input("Select your target coordinates (Ex. A1): ").strip()
+        userShot = input("Select your target coordinates (Ex. A0): ").strip()
 
         try:
             userShotStr = userShot[0].lower()
             if userShotStr == "a" or userShotStr == "b" or userShotStr == "c":
                 userShotInt = int(userShot[1])
-                if userShotInt >= 1 and userShotInt <= 3:
+                if userShotInt >= 0 and userShotInt <= 2:
                     shipCordX = userShotStr
-                    shipCordY = userShotInt - 1
+                    shipCordY = userShotInt 
                     
                     shipCordX = convertLetterNum(shipCordX)
                 
                     ledList = board[shipCordX][shipCordY]
+                    enemyBoard = seenBoard[shipCordX][shipCordY]
                     if (ledList[0] == 1 and ledList[1] == 1) or (ledList[0] == 1 and ledList[1] == 0):
                         raise ZeroDivisionError
                     elif (ledList[0] == 0 and ledList[1] == 0): 
                         print("You have missed the target.")
+                        board[shipCordX][shipCordY][0] = 1
+                        board[shipCordX][shipCordY][1] = 1
+                        enemyBoard[shipCordX][shipCordY][0] = 1
+                        enemyBoard[shipCordX][shipCordY][1] = 1
                     elif ledList[0] == 0 and ledList[1] == 1:
                         print("You have hit a ship!")
+                        board[shipCordX][shipCordY][0] = 1
+                        board[shipCordX][shipCordY][1] = 0
+                        enemyBoard[shipCordX][shipCordY][0] = 1
+                        enemyBoard[shipCordX][shipCordY][1] = 0
 
         except ZeroDivisionError:
             print("You have entered in a location that has already been hit.")
@@ -200,16 +248,15 @@ def atk(board):
 
 
 # Main function
-
 def main():
 
-    matrix()
     player1 = input("What is player 1's name: ")
     redefinition(player1)
     player2 = input("What is player 2's name: ")
     redefinition(player2)
     print("First moves goes to the winner of a coin toss. Player 1 & 2 are heads and tails respectively.")
-    player, = coin_toss()
+    player = coin_toss()
+    pleiades(player)
     # Split turns into player 1 and player 2 turns
     
 
